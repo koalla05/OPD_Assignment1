c18b3d63cd1275357b5fcf4519ea32452adc8ec4
koalla05 <kov.koalla@gmail.com>
2024-09-06 13:48:41 +0300

    class Helper and booking
    Added class Helper to manage  requested functionality. Method book() in Airplane adds new booked tickets as elements of linked lists.

--- main.cpp
+++ main.cpp
@@ -1,47 +1,78 @@
 #include <fstream>
 #include <iostream>
+#include <list>
 #include <map>
 #include <sstream>
 #include <unordered_map>
 #include <vector>
 using namespace std;
 
-class Airplane {
-    //string flight;
-    string date;
-    int numSeat;
-    //vector<int> booking;
-    map<string, string> pricing;
-public:
-    Airplane(string inDate, int inSeatNum, map<string, string> inPricing):
-    date(inDate),  numSeat(inSeatNum), pricing(inPricing){}
-};
-
-class Ticket {
+struct Ticket {
     string userName;
     long id;
+    Ticket(const string& inName, const long& inId): userName(inName), id(inId){}
 };
 
-class Airport {
-    struct pair_hash {
+struct Node {
+    unordered_map<Ticket*, string> data;
+    Node* next;
+
+    Node(unordered_map<Ticket*, string> inData): data(inData), next(nullptr){}
+};
+
+
+class Airplane {
+    string flight;
+    string date;
+    int numSeat, maxSeat;
+    vector<int> booking;
+    vector<list<unordered_map<Ticket*, string>>> linkedLists;
+    map<string, string> pricing;
+    long id;
+public:
+    Airplane(const string& inDate, const string& inFlight, const int& inSeatNum, const map<string, string> inPricing):
+    date(inDate), flight(inFlight), numSeat(inSeatNum), pricing(inPricing), id(1) {
+
+        maxSeat = stoi(pricing.rbegin()->first);
+        for (int i=0; i <maxSeat; i++) {
+            booking.push_back(i);
+            linkedLists.emplace_back();
+        }
+    }
+
+    void book(string seat, string userName) {
+        unordered_map<Ticket*, string> data;
+
+        string row = seat.substr(0, seat.size() - 1);
+        string place = seat.substr(seat.size() - 1);
+
+        Ticket* ticket = new Ticket(userName, id); //made ticket
+
+        data[ticket] = place; //made a map
+        //add this as a list element
+        linkedLists[stoi(row) - 1].push_front(data); //-1 because it starts with zero
+        cout << "Booked row " << row << " place " << place << " id " << id << endl;
+        id++;
+    }
+};
+
+struct pair_hash {
         size_t operator () (const pair<string, string>& p) const {
             size_t h1 = hash<std::string>{}(p.first);
             size_t h2 = hash<std::string>{}(p.second);
-
             return h1 ^ (h2 << 1);
         }
     };
 
-    unordered_map<pair<string, string>, Airplane*, pair_hash> planes;
+class Airport {
 public:
+    unordered_map<pair<string, string>, Airplane*, pair_hash> planes;
     Airport() {}
     void addPlane(string inFlight, string inDate, int inSeatNum, map<string, string> inPricing) {
 
-        planes[make_pair(inDate, inFlight)] = new Airplane (inDate, inSeatNum, inPricing);
+        planes[make_pair(inDate, inFlight)] = new Airplane (inDate, inFlight, inSeatNum, inPricing);
     }
 };
-
-
 
 class FileReader {
     string path = "C:/Users/kovko/CLionProjects/OPD_Assignment1/config.txt";
@@ -83,10 +114,28 @@ public:
     }
 };
 
+
+class Helper {
+    Airport airport;
+public:
+    Helper (Airport& myAirport): airport(myAirport){}
+    void book(const string& date, const string& flight, const string& place, const string& userName) {
+        auto plane = airport.planes[make_pair(date, flight)];
+        if (plane==NULL)
+            cout << "Sorry, no airplane in this day with this flight number :(" << endl;
+        else {
+            plane->book(place, userName);
+        }
+    }
+};
+
 int main()
 {
-    Airport myairport;
-    FileReader file(myairport);
+    Airport myAirport;
+    FileReader file(myAirport);
     file.read();
+    Helper helper(myAirport);
+    helper.book("01.01.2023", "JK321", "1A", "Alla");
+    helper.book("01.01.2023", "JK321", "2B", "JK");
     return 0;
 }
